1345c2e55cefc8ef3ccd424979360e25e0c294cf
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2021-03-04 11:34:16 +0300

    React project is created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import useMediaRecorder from './wmik/use-media-recorder';
+import '../src/App.css';
+function Player({ srcBlob, audio }) {
+  if (!srcBlob) {
+    return null;
+  }
 
-function App() {
+  if (audio) {
+    return <audio src={URL.createObjectURL(srcBlob)} controls />;
+  }
+
+  //burasi player srcBlob olusturuyor
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <video
+      src={URL.createObjectURL(srcBlob)}
+      width={520}
+      height={480}
+      controls
+    />
   );
 }
 
-export default App;
+function ScreenRecorderApp() {
+  let {
+    error,
+    status,
+    mediaBlob,
+    stopRecording,
+    getMediaStream,
+    startRecording
+  } = useMediaRecorder({
+    recordScreen: true,
+    blobOptions: { type: 'video/webm' },
+    mediaStreamConstraints: { audio: true, video: true }
+  });
+
+  return (
+    <article>
+      <div className="header">Screen recorder</div>
+      {error ? `${status} ${error.message}` : status}
+      <section>
+        <div className="button1">
+          <button
+            type="button"
+            onClick={getMediaStream}
+            disabled={status === 'ready'}
+          >
+            Share screen
+        </button>
+        </div>
+        <p className="button2">
+          <button
+            type="button"
+            onClick={startRecording}
+            disabled={status === 'recording'}
+          >
+            Start recording
+        </button>
+        </p>
+        <p className="button3">
+          <button
+            type="button"
+            onClick={stopRecording}
+            disabled={status !== 'recording'}
+          >
+            <i class="far fa-stop-circle"></i>
+          Stop recording
+        </button>
+        </p>
+
+
+      </section>
+
+      <Player srcBlob={mediaBlob} />
+    </article>
+
+  );
+}
+
+export default ScreenRecorderApp;
